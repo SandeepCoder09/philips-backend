e313642d4001d263df9b480fbd5e4b5b8e40dfd6
SandeepCoder09 <161199543+SandeepCoder09@users.noreply.github.com>
2026-02-25 23:02:02 +0530

    Switch to bcryptjs only

--- routes/walletRoutes.js
+++ routes/walletRoutes.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const axios = require("axios");
-const bcrypt = require("bcrypt");
+const bcrypt = require("bcryptjs");
 
 const Transaction = require("../models/Transaction");
 const User = require("../models/User");
